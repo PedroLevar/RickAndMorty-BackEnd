d41a49344d82a5355b6ddc75a553cb54ee808ee7
Antonio Brito <antoniobrito@MacBook-Air-de-Antonio.local>
2025-01-28 21:40:29 -0300

    separacao de responsabilidade

--- src/models/character_model.py
+++ src/models/character_model.py
@@ -11,15 +11,6 @@ class Character(db.Model):
 def __repr__(self):
     return f"<Character {self.name})>"
 
-class CharacterInput(ma.Schema): 
-    name = ma.String(required=True, validate=validate.Length(min=3, max=100))
-    status = ma.String(required=False, validate=validate.Length(max=50), allow_none=True)
-    species = ma.String(required=False, validate=validate.Length(max=100), allow_none=True)
-
-class CharacterInputUpdate(ma.Schema):
-    name = ma.String(required=False, validate=validate.Length(min=3, max=100))
-    status = ma.String(required=False, validate=validate.Length(max=50), allow_none=True)
-    species = ma.String(required=False, validate=validate.Length(max=100), allow_none=True)
 
 class CharacterOutput(ma.Schema):
     id = ma.Integer()
@@ -27,7 +18,5 @@ class CharacterOutput(ma.Schema):
     status = ma.String()
     species = ma.String()
 
-character_input = CharacterInput()
-character_input_update = CharacterInputUpdate()
 character_output = CharacterOutput()
 characters_output = CharacterOutput (many=True)
